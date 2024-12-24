67cd02098d05683ecbef9d3c59e0fa0a49cff597
Ruzin413 <rzmdr413@gmail.com>
2024-12-24 20:15:01 +0545

    Asp.net
    Asp.net

---
+++ Yongbo/Yongbo/Model/Adduser.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace Yongbo.Model
+{
+    public class Adduser
+    {
+        public int Addusers(users user1, IConfiguration conn4)
+        {
+            int i = 0;
+            using (SqlConnection con1 = new SqlConnection(conn4.GetConnectionString("DBCS")))
+            {
+
+                SqlCommand c1 = new SqlCommand("INSERT  INTO Users (Fname,Lname,Email,Password1) VALUES ('" + user1.Fname + "','" + user1.Lname + "','" + user1.Email + "','" + user1.password + "')", con1);
+                {
+                    con1.Open();
+                    i = c1.ExecuteNonQuery();
+                    con1.Close();
+                }
+                return i;
+               
+            }
+        }
+    }
+}
--- Yongbo/Yongbo/Pages/Users/create.cshtml.cs
+++ Yongbo/Yongbo/Pages/Users/create.cshtml.cs
@@ -1,12 +1,46 @@
+using System.Security.Cryptography.Xml;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Yongbo.Model;
 
 namespace Yongbo.Pages.Users
 {
     public class createModel : PageModel
     {
+        public users user= new users();
+        public String showerror = String.Empty;
+        public String showinfo = String.Empty;
+        public readonly IConfiguration configuration;    
+        public createModel(IConfiguration c1)
+        {
+            configuration = c1;
+            
+        }
         public void OnGet()
         {
         }
+        public void OnPost() {
+            user.Fname = Request.Form["fname"];
+            user.Lname = Request.Form["lname"];
+            user.Email = Request.Form["email"];
+            user.password = Request.Form["password"];
+            if (user.Fname.Length < 0 || user.Fname.Length == 0)
+            {
+                showerror = "Fill the given";
+                return;
+            }
+            try{
+                Adduser ad1 = new Adduser();
+                ad1.Addusers(user, configuration);
+
+            }
+            catch
+            {
+                showerror = "data not sent";
+            }
+            showinfo = "data inserted";
+            Response.Redirect("/Users/Index");
+        }
+
     }
 }
